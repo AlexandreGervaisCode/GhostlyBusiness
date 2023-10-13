9c5a9b8094aae7ddfaf67aa8be63d15f6e00db32
Marexillius <alexandregervaisme@gmail.com>
2023-10-12 20:29:46 -0400

    twist missing

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let chaptersObj = {
+    // début
     begin: {
         titre: `L'appel`,
         description: `Vous recevez l'appel d'un client qui souhaite se débarasser d'un fantôme chez lui. Ceci est votre première aventure paranormal, donc vous considèrez faire un appel pour de l'aide. Qui appelez?`,
@@ -10,6 +11,7 @@ let chaptersObj = {
         ]
     },
 
+    // Chapitre 2, choix 1
     confrontationPizza: {
         titre: `Face-à-Face`,
         description: `Après avoir commandé votre récompense pour après l'investigation, vous allez à la maison du client et vous entrez. La porte se ferme derrière vous et vous êtes face-à-face avec le fantôme. Comment survivre ?`,
@@ -21,6 +23,7 @@ let chaptersObj = {
         ]
     },
 
+    // Chapitre 2, choix 2
     confrontationSolo: {
         titre: `Face-à-Face`,
         description: `Après un peu de réflexion, vous déterminez que vous pouvez réussir seul. Vous allez à la maison du client et vous entrez. La porte se ferme derrière vous et vous êtes face-à-face avec le fantôme. Comment survivre ?`,
@@ -32,6 +35,7 @@ let chaptersObj = {
         ]
     },
 
+    // Chapitre 2, choix 3
     confrontationPro: {
         titre: `Face-à-Face`,
         description: `Les meilleurs chasseurs de créatures paranormals vont arriver sous-peu en back-up, en attendent vous allez à la maison du client et vous entrez. La porte se ferme derrière vous et vous êtes face-à-face avec le fantôme. Comment survivre ?`,
@@ -43,6 +47,7 @@ let chaptersObj = {
         ]
     },
 
+    // Chapitre 3
     cachette: {
         titre: `Cache-Cache`,
         description: `Vous fuiez du fantôme, mais il vous poursuis à grande vitesse. Au lieu de continuer à fuir, vous décidez que se cacher serait la meilleure solution pour survire, mais où se cacher ?`,
@@ -54,6 +59,7 @@ let chaptersObj = {
         ]
     },
 
+    // Chapitre temporaire
     compagnon: {
         titre: `Compagnon`,
         description: `Qui avez vous appelez avant votre investigation ?`,
@@ -65,6 +71,7 @@ let chaptersObj = {
         ]
     },
 
+    // Chapitre finale, fin acceptable, si chapitre 2 choix 1
     finPizza: {
         titre: "Fin Acceptable",
         description: "Quelqu'un sonne à la porte. Peu après, le fantôme libère un cri de douleur. Le livreur de pizza a défoncé le fantôme car il n'a pas été donné un pourboire. Malheureusement pour vous, le livreur de pizza se fait payer pour s'avoir occuper du fantôme... Vous avez au moins de la pizza...",
@@ -74,6 +81,7 @@ let chaptersObj = {
         ]
     },
 
+    // Chapitre finale, bonne fin, si chapitre 2 choix 2
     finSolo: {
         titre: "Bonne Fin",
         description: "Vous attendez pour attaquer le fantôme depuis derrière. Il s'enfuit de la maison. Vous avez réussi ! Mais votre client a maintenant une bosse sur la tête...",
@@ -83,6 +91,7 @@ let chaptersObj = {
         ]
     },
 
+    // Chapitre finale, mauvaise fin, si chapitre 2 choix 3
     finPro: {
         titre: "Mauvaise Fin",
         description: "Les professionels arrivent et, croyant que vous êtes le monstre, vous arrêtes. Même dans votre cellule dans la prison, vous entendez le cri du chien des professionnels... SCOOBY-DOO-BY DOOO !",
@@ -92,6 +101,7 @@ let chaptersObj = {
         ]
     },
 
+    // mort chapitre 3
     mortCuisine: {
         titre: "Trouvé !",
         description: "Vous vous cachez derrière un comptoir dans la cuisine, mais, malheureusement pour vous, le fantôme a décidé de vérifiez la cuisine en premier comme salle. Il vous trouve et vous étrangle...",
@@ -101,6 +111,7 @@ let chaptersObj = {
         ]
     },
 
+    // première mort chapitre 2
     mortRien: {
         titre: "...Quoi ?",
         description: "Vous ne faites... rien ? Le fantôme vous étrangle. Vous vous attendiez à quoi exactement ?",
@@ -110,6 +121,7 @@ let chaptersObj = {
         ]
     },
 
+    // deuxième mort chapitre 2
     mortDistraction: {
         titre: "Distraction !",
         description: "Vous tentez de distraire le fantôme en dansant. Peut-être que ça va le rendre confus ! ... il s'approche de vous et vous casse le cou. Vous n'auriez probablement pas avoir fait une dance Fortnite...",
@@ -120,28 +132,60 @@ let chaptersObj = {
     },
 }
 
-function goToChapter(chapterKey) {
-    let chapterTitle = document.querySelector("#chapter");
-    let chapterDescription = document.querySelector("#text");
-    let chapterImg = document.querySelector("#image");
-    let boutons = document.querySelector("#button-container");
-    chapterTitle.innerHTML = chapterKey.titre;
-    chapterDescription.innerHTML = chapterKey.description;
-    chapterImg.src = chapterKey.image;
+// liste de variables
+let chapterTitle = document.querySelector("#chapter");
+let chapterDescription = document.querySelector("#text");
+let chapterImg = document.querySelector("#image");
+// les Twists
+let pizza = false;
+let solo = false;
+let mysteryInc = false;
 
-    while (boutons.firstChild) { 
-        boutons.removeChild(boutons.firstChild); 
-    }
-    
-    for (let i = 0; i < chapterKey.buttons.length; i++) { 
-        const nouveauBtn = document.createElement('button');
-        nouveauBtn.setAttribute("class", "choice");
-        nouveauBtn.textContent = chapterKey.buttons[i].titre;  
-        nouveauBtn.addEventListener('click', () => { 
-        goToChapter(chaptersObj.chapterKey.buttons[i].destination) 
-        }); 
-        boutons.appendChild(nouveauBtn); 
-    };
+function goToChapter(chapterKey) {
+    if (chaptersObj[chapterKey]) {
+        chapterTitle.innerHTML = chaptersObj[chapterKey].titre;
+        chapterDescription.innerHTML = chaptersObj[chapterKey].description;
+        chapterImg.src = chaptersObj[chapterKey].image;
+        
+        if (chaptersObj[chapterKey] == chaptersObj.begin){
+            pizza = false;
+            solo = false;
+            mysteryInc = false;
+        }
+
+        if (chaptersObj[chapterKey] == chaptersObj.confrontationPizza){
+            pizza = true;
+            solo = false;
+            mysteryInc = false;
+        }
+
+        if (chaptersObj[chapterKey] == chaptersObj.confrontationSolo){
+            pizza = false;
+            solo = true;
+            mysteryInc = false;
+        }
+
+        if (chaptersObj[chapterKey] == chaptersObj.confrontationPro){
+            pizza = false;
+            solo = false;
+            mysteryInc = true;
+        }
+
+        const boutons = document.querySelector('#button-container'); 
+		while (boutons.firstChild) { 
+  		boutons.removeChild(boutons.firstChild); 
+		} 
+
+		for (let i = 0; i < chaptersObj[chapterKey].buttons.length; i++) { 
+			const nouveauBtn = document.createElement('button'); 
+            nouveauBtn.setAttribute("class", "choice");
+			nouveauBtn.textContent = chaptersObj[chapterKey].buttons[i].titre; 
+			nouveauBtn.addEventListener('click', function() {
+                goToChapter(chaptersObj[chapterKey].buttons[i].destination);
+            });
+			boutons.appendChild(nouveauBtn); 
+		  }; 
+	}
 }
 
-goToChapter(chaptersObj.begin);
+goToChapter("begin");
